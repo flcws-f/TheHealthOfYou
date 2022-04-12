fd1c491284dc17140eac288be7eeb398d15bdcbf
TheHealthOfYou <103227656+TheHealthOfYou@users.noreply.github.com>
2022-04-13 07:47:40 +0800

    没用

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+System.out.println("- 👋 Hi, I’m TheHealthOfYou.");
+System.out.println("- 🇨🇳 I'm a Chinese man.");
+System.out.println("- 💻 I like Python,HTML,Lua.");
+System.out.println("- 🌱 I'm currently learning.");
+System.out.println("- 📱/💻 I'm programming in the cellphone or computer programming program.");
+  }
+}
