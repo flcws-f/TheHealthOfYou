51a4a64447a80e34941c41c1e8e20b6664148f35
TheHealthOfYou <103227656+TheHealthOfYou@users.noreply.github.com>
2022-08-12 19:24:18 +0800

    Update main.java

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String[] args) {
 System.out.println("- 👋 Hi, I’m TheHealthOfYou.");
 System.out.println("- 🇨🇳 I'm a Chinese man.");
-System.out.println("- 💻 I like Python,HTML,Lua.");
+System.out.println("- 💻 I like Python,HTML,VBS,CSS.");
 System.out.println("- 🌱 I'm currently learning.");
 System.out.println("- 📱/💻 I'm programming in the cellphone or computer programming program.");
   }
